95286ca26f88c39c3e6ee4460c517a737b5fe9a4
Simon Escano <escanosimonlyster@gmail.com>
2024-04-22 22:55:59 +0800

    auto restructure

---
+++ cmake-build-debug/CMakeFiles/AVL_Tree.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "AVL_Tree.exe"
+  "AVL_Tree.exe.manifest"
+  "AVL_Tree.pdb"
+  "CMakeFiles/AVL_Tree.dir/main.cpp.obj"
+  "CMakeFiles/AVL_Tree.dir/main.cpp.obj.d"
+  "libAVL_Tree.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AVL_Tree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/AVL_Tree.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for AVL_Tree.
+# This may be replaced when dependencies are built.
